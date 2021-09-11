121a1759d3576c2c658b7b327719a832e684303e
ojb44 <70651278+ojb44@users.noreply.github.com>
2021-09-11 14:00:37 +0100

    Improved init functions.
    Can now initiate from either sourceArray, configVector, or sourceTuples.

--- configOptimization.py
+++ configOptimization.py
@@ -1,6 +1,6 @@
 import sigmaRMSSquFuncs
 import customOptimizerSettings
-import aLValueFuncs
+from aLValueFuncs import *
 import imperfectionFuncs
 import rdofSigmaRMSSquFuncs
 import numpy as np
@@ -20,17 +20,40 @@ class SingleSource:
         return (self.theta,self.phi,self.strength)
         
 class Configuration: #rename to SourceConfiguration
-    def __init__(self, numSources, sourceType, sourceArray):
-        if numSources==np.size(sourceArray):  #kept in as check (that got right num of sources in array!)
-            #self.sourceArray=sourceArray
-            self.numSources=numSources
-            self.sourceType=sourceType
-            self.configVector=self.configVectorFromSourceArray(sourceArray)
+    def __init__(self, numSources, sourceType, sourceArray=None, configVector=None, sourceTuples=None):
+        self.numSources=numSources
+        self.sourceType=sourceType
+        
+        if (sourceArray is None) and (configVector is None) and (sourceTuples is None):
+            self.configVector=self.randomConfigVector()
             
+        elif configVector is None and sourceTuples is None: #initialize from sourceArray
+            if self.numSources==np.size(sourceArray):  #kept in as check (that got right num of sources in array!)
+                self.configVector=self.configVectorFromSourceArray(sourceArray)
+                
+            else:
+                #sourceArray
+                raise ValueError('Number of sources did not match size of array')
+                
+        elif sourceArray is None and sourceTuples is None: #initialize from configVector
+            if 3*self.numSources==np.size(configVector):  
+                self.configVector=configVector
+                
+            else:
+                #configVector
+                raise ValueError('Number of sources did not match size of array')
+                
+        elif sourceArray is None and configVector is None: #initialize from sourceTuples
+            if 3*self.numSources==np.size(sourceTuples):  #kept in as check (that got right num of sources in array!)
+                self.configVector=self.configVectorFromSourceTuples(sourceTuples)
+                
+            else:
+                #sourceTuples
+                raise ValueError('Number of sources did not match size of array')
+                
         else:
-            raise ValueError('Number of sources did not match size of array')
+            raise ValueError('Arguments were not provided correctly.')
     
-    #have seperate init from configVector
             
     def configVectorFromSourceArray(self,sourceArray):
         thetas=list(map(lambda x: x.theta, sourceArray))
@@ -38,11 +61,20 @@ class Configuration: #rename to SourceConfiguration
         strengths=list(map(lambda x: x.strength, sourceArray))
         return np.array([thetas,phis,strengths]).flatten()
     
+    def configVectorFromSourceTuples(self, sourceTuples):
+        return np.array(sourceTuples).T.flatten()
+    
     def sourceArrayFromConfigVector(self):
         sourceArray=np.empty(self.numSources,dtype=SingleSource)
         for i in range(self.numSources):
             sourceArray[i]=SingleSource(self.configVector[i],self.configVector[i+self.numSources],self.configVector[i+2*self.numSources])
         return sourceArray
+    
+    def randomConfigVector(self):
+        thetas=np.random.rand(self.numSources)*(np.pi)
+        phis=np.random.rand(self.numSources)*(np.pi*2)
+        strengths=np.ones(self.numSources) #all same strength
+        return np.concatenate((thetas,phis,strengths))
     
     def sourceTuples(self):
         sourceArray=self.sourceArrayFromConfigVector()
@@ -64,17 +96,40 @@ class Configuration: #rename to SourceConfiguration
         return sigmaRMSSquFuncs.sigmaRMSSqu(configVector,self.numSources,self.sourceType.aLVals,numLTerms), sigmaRMSSquFuncs.sigmaRMSSquGrad(configVector,self.numSources,self.sourceType.aLVals,numLTerms)
 
 
-class ConfigurationRDOF(Configuration): #rename
-    def __init__(self, numSources, sourceType, halfSourceArray):
-        if int(numSources/2)==np.size(halfSourceArray):
-            self.numSources=numSources
-            self.sourceType=sourceType
-            self.configVector=super().configVectorFromSourceArray(halfSourceArray)
+class ConfigurationRDOF(Configuration): #rename      
+    def __init__(self, numSources, sourceType, sourceArray=None, configVector=None, sourceTuples=None):
+        self.numSources=numSources
+        self.sourceType=sourceType
         
-        else:
-            raise ValueError('Number of sources was not twice size of array')
+        if (sourceArray is None) and (configVector is None) and (sourceTuples is None):
+            self.configVector=self.randomConfigVector()
             
-
+        elif configVector is None and sourceTuples is None: #initialize from sourceArray
+            if int(numSources/2)==np.size(sourceArray):  #kept in as check (that got right num of sources in array!)
+                self.configVector=self.configVectorFromSourceArray(sourceArray)
+                
+            else:
+                #sourceArray
+                raise ValueError('Number of sources was not twice size of array')
+                
+        elif sourceArray is None and sourceTuples is None: #initialize from configVector
+            if 3*int(numSources/2)==np.size(configVector):  
+                self.configVector=configVector
+                
+            else:
+                #configVector
+                raise ValueError('Number of sources was not twice size of array')
+                
+        elif sourceArray is None and configVector is None: #initialize from sourceTuples
+            if 3*int(numSources/2)==np.size(sourceTuples):  #kept in as check (that got right num of sources in array!)
+                self.configVector=self.configVectorFromSourceTuples(sourceTuples)
+                
+            else:
+                #sourceTuples
+                raise ValueError('Number of sources was not twice size of array')
+                
+        else:
+            raise ValueError('Arguments were not provided correctly.')
 
     def fullConfigVector(self):
         fullConfigVec=np.zeros(3*self.numSources)
@@ -87,6 +142,12 @@ class ConfigurationRDOF(Configuration): #rename
             fullConfigVec[i+4*halfNumSources]=self.configVector[i+2*halfNumSources]
             fullConfigVec[i+5*halfNumSources]=self.configVector[i+2*halfNumSources]
         return fullConfigVec
+    
+    def randomConfigVector(self):
+        thetas=np.random.rand(int(self.numSources/2))*(np.pi)
+        phis=np.random.rand(int(self.numSources/2))*(np.pi*2)
+        strengths=np.ones(int(self.numSources/2)) #all same strength
+        return np.concatenate((thetas,phis,strengths))
     
     def fullSourceArray(self):
         fullConfigVec=self.fullConfigVector()
@@ -110,8 +171,6 @@ class ConfigurationRDOF(Configuration): #rename
         
     def sigmaRMSSquGrad(self,numLTerms):
         return rdofSigmaRMSSquFuncs.sigmaRMSSquGradRDOF(self.configVector,int(self.numSources/2),self.sourceType.aLVals,numLTerms)
-        
-    #sigmaRMSPercent func
 
     def minimizerFunction(self, configVector, numLTerms):
         return rdofSigmaRMSSquFuncs.sigmaRMSSquRDOF(configVector,int(self.numSources/2),self.sourceType.aLVals,numLTerms),rdofSigmaRMSSquFuncs.sigmaRMSSquGradRDOF(configVector,int(self.numSources/2),self.sourceType.aLVals,numLTerms)
@@ -123,36 +182,21 @@ class Optimization:
         self.gtol=gtol
         self.temp=temp
         self.numLTerms=numLTerms
-        self.mybounds=customOptimizerSettings.MyBounds(self.configuration.numSources)
-        self.mytakestep=customOptimizerSettings.MyTakeStep(self.configuration.numSources,stepsize=maxStepSize)
-        self.mytakestepRDOF=customOptimizerSettings.MyTakeStepRDOF(int(self.configuration.numSources/2),stepsize=maxStepSize)
-        self.myboundsRDOF=customOptimizerSettings.MyBounds(int(self.configuration.numSources/2))
+        if isinstance(self.configuration, ConfigurationRDOF)==False:
+            self.mybounds=customOptimizerSettings.MyBounds(self.configuration.numSources)
+            self.mytakestep=customOptimizerSettings.MyTakeStep(self.configuration.numSources,stepsize=maxStepSize)
+        else:
+            self.mytakestep=customOptimizerSettings.MyTakeStepRDOF(int(self.configuration.numSources/2),stepsize=maxStepSize)
+            self.mybounds=customOptimizerSettings.MyBounds(int(self.configuration.numSources/2))
         self.localMinKwargs={"method":"L-BFGS-B", "jac":True, "args":(self.numLTerms),"options":{'gtol':self.gtol, 'disp':False}}
 
-    # #@njit
-    # def minimizerFunction(self,configVector):
-    #     #return self.configuration.sigmaRMSSqu(self.numLTerms),self.configuration.sigmaRMSSquGrad(self.numLTerms)
-    #     return sigmaRMSSquFuncs.sigmaRMSSqu(configVector,self.configuration.numSources,self.configuration.sourceType.aLVals,self.numLTerms), sigmaRMSSquFuncs.sigmaRMSSquGrad(configVector,self.configuration.numSources,self.configuration.sourceType.aLVals,self.numLTerms)
-        
-    # def minimizerFunctionRDOF(self,configVector):
-    #     return rdofSigmaRMSSquFuncs.sigmaRMSSquRDOF(configVector,int(self.configuration.numSources/2),self.configuration.sourceType.aLVals,self.numLTerms),rdofSigmaRMSSquFuncs.sigmaRMSSquGradRDOF(configVector,int(self.configuration.numSources/2),self.configuration.sourceType.aLVals,self.numLTerms)
-    # #figure out way to put minimizerFunctions in the configuration objects - pass the numLTerms as an argument
-    
     
     def optimize(self, numIterations):
         result=basinhopping(self.configuration.minimizerFunction,self.configuration.configVector,minimizer_kwargs=self.localMinKwargs,niter=numIterations,T=self.temp,disp=True,accept_test=self.mybounds,take_step=self.mytakestep)
         self.configuration.configVector=np.array(list(result.x))
         return self.configuration.configVector
     
-    # def optimize(self, numIterations):
-    #     if isinstance(self.configuration, ConfigurationRDOF)==False:
-    #         result=basinhopping(self.minimizerFunction,self.configuration.configVector,minimizer_kwargs=self.localMinKwargs,niter=numIterations,T=self.temp,disp=True,accept_test=self.mybounds,take_step=self.mytakestep)
-    #         self.configuration.configVector=np.array(list(result.x))
-    #         return self.configuration.configVector
-    #     else:
-    #         result=basinhopping(self.minimizerFunctionRDOF,self.configuration.configVector,minimizer_kwargs=self.localMinKwargs,niter=numIterations,T=self.temp,disp=True,accept_test=self.myboundsRDOF,take_step=self.mytakestepRDOF)
-    #         self.configuration.configVector=np.array(list(result.x))
-    #         return self.configuration.configVector
+
 
 
 
